634cba99f5b2b576d6d8b99e03ced25c643b755e
nikki-62 <101091625+nikki-62@users.noreply.github.com>
2022-06-24 11:38:00 +0530

    Update Multiply.java

--- Multiply.java
+++ Multiply.java
@@ -1,10 +1,10 @@
 class Multiply{
 
        public static void main(String[]args){
-         int x=23;
+         int x=23;   //integer data type.
          int y=43;
-         int Mul=x+y;
+         int Mul=x+y;   //multiplication of x and y.
 
-          System.out.println("Mul of the Two Number are: "+Mul);
+          System.out.println("Mul of the Two Number are: "+Mul); //print the multiplication.
 }
 }
