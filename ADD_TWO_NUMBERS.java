34a847dad6d40dde88201ec7600cb6934f62c31a
nikki-62 <101091625+nikki-62@users.noreply.github.com>
2022-06-24 11:36:58 +0530

    Update ADD_TWO_NUMBERS.java

--- ADD_TWO_NUMBERS.java
+++ ADD_TWO_NUMBERS.java
@@ -2,9 +2,9 @@ class ADD_TWO_NUMBERS{
      
         public static void main(String[]args){
              
-              int X=23;
+              int X=23;     // integer data type
               int Y=45;
-              int Sum = X+Y;
-            System.out.println("Total Number is: "+Sum);
+              int Sum = X+Y;  //use add two number
+            System.out.println("Total Number is: "+Sum);  //print the total sum.
 }
 }
